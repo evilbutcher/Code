e5e33acbbddde2e6dd3ddcbddcd41d3291533342
evilbutcher <pgcrfhht@outlook.com>
2020-10-05 19:33:06 +0800

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,41 @@
+/*
+ * Author: evilbutcher
+ * Github: https://github.com/evilbutcher
+ */
+
+module.exports = {
+  bilibili: () => 0, //👈就改这个数字。B站榜单对应关系：0全站，1动画，3音乐，4游戏，5娱乐，36科技，119鬼畜，129舞蹈。
+  blnum: () => 6, //自定义B站显示数量
+  blrancolor: () => true, //是否开启B站随机颜色
+  dbnum: () => 6, //自定义豆瓣显示数量
+  dbrancolor: () => true, //是否开启豆瓣随机颜色
+  rrnum: () => 6, //自定义人人影视显示数量
+  rrrancolor: () => true, //是否开启人人影视随机颜色
+  rsslink: () =>
+    "https://github.com/evilbutcher/Quantumult X/commits/master.atom", //👈就改引号里的链接。
+  rssnum: () => 6, //自定义RSS显示数量
+  rssrancolor: () => true, //是否开启RSS随机颜色
+  wbnum: () => 6, //自定义微博显示数量
+  wbrancolor: () => true, //是否开启微博随机颜色
+  zhnum: () => 6, //自定义知乎显示数量
+  zhrancolor: () => true, //是否开启知乎随机颜色
+  nasaapi: () => "", //填写NASA API Key
+  imglink: () => "http://api.dujin.org/bing/1366.php", //NASA备用图片地址，可自定义
+  imglink2: () => "https://source.unsplash.com/random", //NASA备用图片地址，可自定义
+  checkintitle: () => "", //填写签到标题
+  checkinloginurl: () => "", //填写签到登陆链接
+  checkinemail: () => "", //填写签到邮箱
+  checkinpwd: () => "", //填写签到密码
+  apps: () => [
+    "1443988620|hk",
+    "1312014438 cn",
+    "499470113/vn",
+    "1314212521-jp",
+    "1282297037_au",
+    "932747118:ie",
+    "1116905928",
+    "1373567447",
+  ], //app跟踪id
+  reg: () => "cn", //默认区域：美国us 中国cn 香港hk
+  lastnumberofcar: () => "3" //北京尾号限行脚本填写汽车尾号
+};
