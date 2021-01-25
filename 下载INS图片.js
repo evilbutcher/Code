65fd95e2b45d7181d3baa711dedd8d7f32a5271a
evilbutcher <pgcrfhht@outlook.com>
2021-01-25 16:39:52 +0800

    Update 下载INS图片.js

--- 下载INS图片.js
+++ 下载INS图片.js
@@ -7,16 +7,17 @@ $input.text({
     if (link.length == 0) {
       link = L;
     }
+    $ui.toast("正在获取下载链接...");
     $http.get({
       url: link,
       header: {
         host: "www.instagram.com",
         useragent:
-          "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
-        cookie:
-          "自行抓包此处填cookie"
+          "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+        cookie: "sessionid=抓包填入;"
       },
       handler: function(resp) {
+        $ui.toast("正在解析链接...");
         var data = resp.data;
         var search = new RegExp(/display_url":".*?"/);
         var img = search.exec(data);
@@ -27,13 +28,16 @@ $input.text({
         var imglink = img3.slice(0, -4);
         $http.download({
           url: imglink,
+          backgroundFetch: true, // Optional, default is false
+          progress: function(bytesWritten, totalBytes) {
+            var percentage = (bytesWritten * 1.0) / totalBytes;
+            $ui.toast(`下载中(${parseInt(percentage * 100)}%)`);
+          },
           handler: function(resp) {
             $photo.save({
               data: resp.data,
               handler: function(success) {
-                $push.schedule({
-                  title: "success"
-                });
+                $ui.success("下载成功");
               }
             });
           }
