d34dc691a20e58cf44cca58dc1e29c308a8fe769
evilbutcher <pgcrfhht@outlook.com>
2022-07-01 19:48:29 +0800

    Update 经纬度转坐标.js

--- 经纬度转坐标.js
+++ 经纬度转坐标.js
@@ -1,19 +1,30 @@
 //直接复制求助短信，运行脚本即可使用
 var text = $clipboard.text;
 if (text != undefined) {
-  var getw = /北纬\:\d+\°\d+\′\d+\.\d+″/;
-  var gete = /东经\:\d+\°\d+\′\d+\.\d+″/;
+  var getw = /北纬\:\d+°\d+′\d+\.\d+″/;
+  var getwd = /北纬\:\d+°/;
+  var getm = /°\d+′/;
+  var gets = /′\d+\.\d+″/;
+  var gete = /东经\:\d+°\d+′\d+\.\d+″/;
+  var geted = /东经\:\d+°/;
   var prew = text.match(getw);
   var pree = text.match(gete);
   if (prew != undefined && pree != undefined) {
-    var nd = JSON.stringify(prew).slice(5, 7);
-    var nm = JSON.stringify(prew).slice(8, 10);
-    var ns = JSON.stringify(prew).slice(11, -3);
+    var prend = JSON.stringify(prew).match(getwd);
+    var nd = JSON.stringify(prend).slice(5, -3);
+    var prenm = JSON.stringify(prew).match(getm);
+    var nm = JSON.stringify(prenm).slice(3, -3);
+    var prens = JSON.stringify(prew).match(gets);
+    var ns = JSON.stringify(prens).slice(3, -3);
     var n = parseFloat(nd) + parseFloat(nm) / 60 + parseFloat(ns) / 3600;
     console.log(n);
-    var ed = JSON.stringify(pree).slice(5, 8);
-    var em = JSON.stringify(pree).slice(9, 11);
-    var es = JSON.stringify(pree).slice(12, -3);
+    var preed = JSON.stringify(pree).match(getwd);
+    var preed = JSON.stringify(pree).match(geted);
+    var ed = JSON.stringify(preed).slice(5, -3);
+    var preem = JSON.stringify(pree).match(getm);
+    var em = JSON.stringify(preem).slice(3, -3);
+    var prees = JSON.stringify(pree).match(gets);
+    var es = JSON.stringify(prees).slice(3, -3);
     var e = parseFloat(ed) + parseFloat(em) / 60 + parseFloat(es) / 3600;
     console.log(e);
     $http.request({
@@ -26,7 +37,7 @@ if (text != undefined) {
         Referer: `https://www.piliang.tech/`,
         Host: `api.map.baidu.com`,
         "User-Agent": `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15`,
-        "Accept-Language": `zh-CN,zh-Hans;q=0.9`
+        "Accept-Language": `zh-CN,zh-Hans;q=0.9`,
       },
       handler: function (resp) {
         var data = JSON.parse(resp.data.slice(35, -1));
@@ -45,20 +56,20 @@ if (text != undefined) {
         }
         $push.schedule({
           title: "经纬度转坐标",
-          body: "参考地址：" + address + "\n附近地点名称：" + name
+          body: "参考地址：" + address + "\n附近地点名称：" + name,
         });
         console.log(result);
-      }
+      },
     });
   } else {
     $push.schedule({
       title: "经纬度转坐标",
-      body: "未查询到经纬度坐标"
+      body: "未查询到经纬度坐标",
     });
   }
 } else {
   $push.schedule({
     title: "经纬度转坐标",
-    body: "剪贴板为空"
+    body: "剪贴板为空",
   });
 }
